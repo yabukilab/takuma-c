83bb35ea6019cc806b3b732fed42d0a80b5913bb
TotaKikuti <s1942033VQ@s.chibakoudai.jp>
2021-07-15 03:45:21 +0900

    fcccxzvsdrzrgse

--- htdocs/検索画面.php
+++ htdocs/検索画面.php
@@ -13,7 +13,13 @@ $username = $_SESSION['username']; // ユーザ名を思い出す．
   <title>メンバページ</title>
 </head>
 <body>
-  <p><?php echo $username;?>のためのメンバページ</p>
+  <p><?php echo $username;?>さん，ようこそ</p>
+  <p>現在の気分を選択してください</p>
+  <input type="image" src="btn_send1.png" width="224" height="224" alt="喜" value="喜" name="submit1">
+  <input type="image" src="btn_send2.png" width="224" height="224" alt="怒" value="怒" name="submit2">
+  <input type="image" src="btn_send3.png" width="224" height="224" alt="哀" value="哀" name="submit3">
+  <input type="image" src="btn_send4.png" width="224" height="224" alt="楽" value="楽" name="submit4">
+
   <p><a href="ログアウト.php">ログアウト</a></p>
 </body>
 </html>

57762a70f75c214b1e7c00bf65716d71de3f6920
TotaKikuti <s1942033VQ@s.chibakoudai.jp>
2021-07-15 04:29:24 +0900

    s

--- htdocs/検索画面.php
+++ htdocs/検索画面.php
@@ -4,6 +4,30 @@ if (!isset($_SESSION['username'])) { // ログインしていないなら，
   header('Location: ログイン.php');     // ログインページへ転送する．
 }
 $username = $_SESSION['username']; // ユーザ名を思い出す．
+
+if (isset($_POST['submit1'], $_POST['submit2'],$_POST['submit3'],$_POST['submit4'])) {
+  $submit = $_POST['submit1']; 
+  $submit = $_POST['submit2']; 
+  $submit = $_POST['submit3']; 
+  $submit = $_POST['submit4']; 
+  
+  if ($submit == 'submit1') {     
+    $_SESSION['submit1'] = true;      
+    header('Location: 検索結果.php'); 
+  } else if($submit == 'submit2'){ 
+    $_SESSION['submit2'] = true;      
+    header('Location: 検索結果.php'); 
+  } else if($submit == 'submit3'){ 
+    $_SESSION['submit3'] = true;      
+    header('Location: 検索結果.php'); 
+  }else{
+    $_SESSION['submit4'] = true; 
+    header('Location: 検索画面.php'); 
+  }
+} // ユーザ名とパスワードが送信されていないなら以下のフォームを表示する．
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">

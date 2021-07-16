25b3a7efee1e3b8bc4fcc76fc7485fa5e1616144
TotaKikuti <s1942033VQ@s.chibakoudai.jp>
2021-07-16 12:40:30 +0900

    zzz

--- htdocs/検索結果.php
+++ htdocs/検索結果.php
@@ -14,19 +14,19 @@
 <th>musicURL</th>
 </tr>
     <?php require 'db.php';
+        $t = intval(date('H'));
+        if(5 <= $t && $t <= 11 ){
+            $NT = "朝";
+            }else if(12 <= $t && $t <= 17){
+                $NT = "昼";
+                }else{
+                    $NT = "夜";
+                    }?>
+        <?php $feeling = $_POST["q1"];
        $sql = 'SELECT * FROM music_data where feeling = "'.$feeling.'" and time = "'.$NT.'"';
        $prepare = $db->prepare($sql);
        $prepare->execute();
-       $result = $prepare->fetchAll(PDO::FETCH_ASSOC); 
-    $t = intval(date('H'));
-    if(5 <= $t && $t <= 11 ){
-        $NT = "朝";
-        }else if(12 <= $t && $t <= 17){
-            $NT = "昼";
-            }else{
-                $NT = "夜";
-                }?>
-    <?php $feeling = $_POST["q1"];?>
+       $result = $prepare->fetchAll(PDO::FETCH_ASSOC); ?>
     <?php 
     foreach ($result as $row) {
         $musicID = h($row['musicID']);

fe34de1927fe647d4ef29c98703b3664f6accaaa
TotaKikuti <s1942033VQ@s.chibakoudai.jp>
2021-07-16 09:22:00 +0900

    xxxx

--- htdocs/検索結果.php
+++ htdocs/検索結果.php
@@ -15,7 +15,7 @@ if(5 <= $t && $t <= 11 ){
 <?php require 'db.php'; ?># 接続
 //$db = new PDO($dsn, $dbUser, $dbPass);
 
-<?php $sql = "SELECT * FROM music_data where feeling = "'.$feeling.'" and time = "'.$NT.'";
+<?php $sql = "SELECT * FROM music_data where feeling = "'.$feeling.'" and time = "'.$NT.'"";
 $prepare = $db->prepare($sql);
 $prepare->execute();
 $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
@@ -23,7 +23,7 @@ $prepare->bindValue(':name', $name, PDO::PARAM_STR); ?>
 
 <?php 
 foreach ($result as $r) {
-  echo $r['musicID'].' '.$r['musicname'].' '.$r['arthist'].''.$r['musicURL'].';   
+  echo $r['musicID'].' '.$r['musicname'].' '.$r['arthist'].''.$r['musicURL'].;   
 
   echo "<br/>";
 }

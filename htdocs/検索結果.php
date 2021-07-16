35b81039edafb277b89af9832d4f30e2795b3b04
TotaKikuti <s1942033VQ@s.chibakoudai.jp>
2021-07-16 12:38:06 +0900

    s

--- htdocs/検索結果.php
+++ htdocs/検索結果.php
@@ -13,7 +13,11 @@
 <th>arthist</th>
 <th>musicURL</th>
 </tr>
-    <?php
+    <?php require 'db.php';
+       $sql = 'SELECT * FROM music_data where feeling = "'.$feeling.'" and time = "'.$NT.'"';
+       $prepare = $db->prepare($sql);
+       $prepare->execute();
+       $result = $prepare->fetchAll(PDO::FETCH_ASSOC); 
     $t = intval(date('H'));
     if(5 <= $t && $t <= 11 ){
         $NT = "朝";
@@ -23,11 +27,6 @@
                 $NT = "夜";
                 }?>
     <?php $feeling = $_POST["q1"];?>
-    <?php require 'db.php'; ?>
-    <?php $sql = 'SELECT * FROM music_data';
-    $prepare = $db->prepare($sql);
-    $prepare->execute();
-    $result = $prepare->fetchAll(PDO::FETCH_ASSOC); ?>
     <?php 
     foreach ($result as $row) {
         $musicID = h($row['musicID']);

e5dbe8014906937a18d893c5f18828db11e1dc37
TotaKikuti <s1942033VQ@s.chibakoudai.jp>
2021-07-16 09:35:29 +0900

    xxxxxxx

--- htdocs/検索結果.php
+++ htdocs/検索結果.php
@@ -1,30 +1,30 @@
-<?php
-$t = intval(date('H'));
-
-
-if(5 <= $t && $t <= 11 ){
-    $NT = "朝";?>
-<?php }else if(12 <= $t && $t <= 17 ){
-    $NT = "昼";?>
-<?php}else{
-    $NT = "夜";?>
-<?php}?>
-<?php print ($_POST["q1"]);?>
-<?php $feeling = $_POST["q1"];?>
-
-<?php require 'db.php'; ?># 接続
-//$db = new PDO($dsn, $dbUser, $dbPass);
-
-<?php $sql = 'SELECT * FROM music_data where feeling = "'.$feeling.'" and time = "'.$NT.'"';
-$prepare = $db->prepare($sql);
-$prepare->execute();
-$result = $prepare->fetchAll(PDO::FETCH_ASSOC);
-$prepare->bindValue(':name', $name, PDO::PARAM_STR); ?>
-
-<?php 
-foreach ($result as $r) {
-  echo $r['musicID'].' '.$r['musicname'].' '.$r['arthist'].''.$r['musicURL'];  
-
-  echo "<br/>";
-}
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset='utf-8' />
+  <link rel='stylesheet' href='style.css' />
+  <title>メンバページ</title>
+</head>
+<body> 
+    <?php
+    $t = intval(date('H'));
+    if(5 <= $t && $t <= 11 ){$NT = "朝";?>
+    <?php}else{
+        $NT = "夜";?>
+        <?php}?>
+    <?php print ($_POST["q1"]);?>
+    <?php $feeling = $_POST["q1"];?>
+    <?php require 'db.php'; ?># 接続
+    //$db = new PDO($dsn, $dbUser, $dbPass);
+    <?php $sql = 'SELECT * FROM music_data where feeling = "'.$feeling.'" and time = "'.$NT.'"';
+    $prepare = $db->prepare($sql);
+    $prepare->execute();
+    $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+    $prepare->bindValue(':name', $name, PDO::PARAM_STR); ?>
+    <?php 
+    foreach ($result as $r) {
+        echo $r['musicID'].' '.$r['musicname'].' '.$r['arthist'].''.$r['musicURL'];  
+        echo "<br/>";
+    }?>
+</body>
+</html>
